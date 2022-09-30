c8b46338694012ba734b2672c307220e54877997
nullptrmachine <68807660+nullptrmachine@users.noreply.github.com>
2022-09-30 23:47:16 +0200

    Create Wave.h

---
+++ Wave.h
@@ -0,0 +1,51 @@
+/* ██████╗  █████╗ ███████╗████████╗███████╗██████╗	 */
+/* ██╔══██╗██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗ */
+/* ██████╔╝███████║███████╗   ██║   █████╗  ██║  ██║ */
+/* ██╔═══╝ ██╔══██║╚════██║   ██║   ██╔══╝  ██║  ██║ */
+/* ██║     ██║  ██║███████║   ██║   ███████╗██████╔╝ */
+/* ╚═╝     ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═════╝	 */
+
+#ifndef _WAVE_H
+#define _WAVE_H
+
+#pragma once
+
+#include <windows.h>
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+#include <mmsystem.h>
+
+class WaveIn {
+public:
+	WaveIn();
+	~WaveIn();
+
+	BOOL open(int channels, int freq, int bits, HWND hWnd);
+	void close();
+	BOOL addBuffer(size_t size);
+	BOOL start();
+
+private:
+	HWAVEIN hWaveIn;
+	WAVEFORMATEX wfex;
+	std::list<void*> waveHeaders;
+};
+
+class WaveOut {
+public:
+	WaveOut();
+	~WaveOut();
+
+	BOOL open(int channels, int freq, int bits, HWND hWnd, int deviceID = WAVE_MAPPER);
+	void close();
+	void play(const char* buf, size_t size);
+
+private:
+	HWAVEOUT hWaveOut;
+	WAVEFORMATEX wfex;
+};
+
+#endif
